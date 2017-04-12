612635b118bb8f55e7b800a764267333cf78bf55
Bruno Gonçalves <btoztas@gmail.com>
2017-04-12 14:48:40 +0100

    Implementing deckofcards

--- VideoPoker/VideoPoker/src/deckofcards/Card.java
+++ VideoPoker/VideoPoker/src/deckofcards/Card.java
@@ -3,13 +3,23 @@ package deckofcards;
 public class Card {
 	
 	char rank;
-	String suit;
+	char suit;
 
 
-	Card(char rank, String suit){
+	Card(char rank, char suit){
 		
 		this.rank = rank;
 		this.suit = suit;
+	}
+
+
+	public char getRank() {
+		return rank;
+	}
+
+
+	public char getSuit() {
+		return suit;
 	}
 
 }
--- VideoPoker/VideoPoker/src/deckofcards/Deck.java
+++ VideoPoker/VideoPoker/src/deckofcards/Deck.java
@@ -1,5 +1,28 @@
 package deckofcards;
 
-public class Deck {
+import java.util.ArrayList;
 
+public class Deck {
+	
+	ArrayList<Card> deck = new ArrayList<Card>();
+	private char[] suits = {'H', 'S', 'D', 'C'};
+	private char[] ranks = {'2', '3', '4', '5', '6', '7', '8', '9', 'D', 'J', 'Q', 'K', 'A'};
+	
+	
+	
+	Deck(){
+		
+		for (char suit : suits) {
+			for(char rank: ranks){
+				Card card = new Card(rank, suit);
+				deck.add(card);
+			}
+		}	
+	}
+	
+	
+	
+	
+	
+	
 }
