a1fe09403093db1c4e85072f3ed5171be2f9a3ca
Afonso Costa <afonside@hotmail.com>
2017-05-06 20:32:31 +0100

    started exceptions catch

--- VideoPoker/src/gameUI/Interactive.java
+++ VideoPoker/src/gameUI/Interactive.java
@@ -20,8 +20,9 @@ public class Interactive implements GameMode {
 	public void play() {
 		String s = "";
 		VideoPoker v = new VideoPoker(credit, new VideoPokerType107DB());
-		int amount=0;
+		
 		BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
+		
 		while(true){
 			try{
 	        s = bufferRead.readLine();
@@ -33,16 +34,19 @@ public class Interactive implements GameMode {
 			String[] tokens = s.split("\\s+\\,\\.");
 			if(s.contains("b")){
 				if(s.length()==1){
-					v.bet(5);
-					amount=5;
-					System.out.println("player is betting 5");
+					try{
+						v.bet(5);
+						System.out.println("player is betting 5");
+					}catch(InvalidAmountException e1){
+						throw e1;
+					}catch(InvalidGameStateException e2){
+						throw e2;
+					}
 				}else{
 					v.bet(Integer.parseInt(tokens[2]));
-					amount=Integer.parseInt(tokens[2]);
 					System.out.println("player is betting " + tokens[2]);
 				}
 			}else if(s.contains("d")){
-    			v.bet(amount);
 				System.out.println(v.deal());
 			}else if(s.contains("h")){
 				int[] h = new int[(s.length()-1)/2];
