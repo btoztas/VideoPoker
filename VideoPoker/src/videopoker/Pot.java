d294835bb71d03b3a3a3b8f902ecacbcc0406809
Afonso Costa <afonside@hotmail.com>
2017-05-09 14:26:00 +0100

    comments on videopoker package, missing exceptions and VideoPoker class

--- VideoPoker/src/videopoker/Pot.java
+++ VideoPoker/src/videopoker/Pot.java
@@ -4,13 +4,28 @@ public class Pot {
 	
 	private int value;
 	
+	/**
+	 * Constructs an object of type Pot. Initializes the object value with 0
+	 */
+	
 	Pot(){
 		value = 0;
 	}
 	
+	/**
+	 * This method adds to the pot the value of the bet given by the parameter bet
+	 * @param bet		amount to add to the pot
+	 */
+	
 	void add(int bet){
 		value = bet;
 	}
+	
+	/**
+	 * This method withdraws from the pot the value present in it and returns it
+	 * @param out		amount of money that was in the pot
+	 */
+	
 	int withdraw(){
 		int out = value;
 		value = 0;
--- VideoPoker/src/videopoker/VideoPokerType.java
+++ VideoPoker/src/videopoker/VideoPokerType.java
@@ -1,10 +1,41 @@
 package videopoker;
 
 public interface VideoPokerType {
-
+	
+	/**
+	 * This method allows user to implement its own way to evaluate a given hand according to the type
+	 * of poker played
+	 * @param hand		hand to be evaluated
+	 * @return handtype		type of the hand being analyzed
+	 */
+	
 	String evaluateHand(Hand hand);
+	
+	/**
+	 * This method allows user to implement its own payout values according to the type of videopoker played. Given a
+	 * hand type and a bet this method returns the correspondent payout
+	 * @param handtype		type of the hand being analyzed
+	 * @param bet			amount of money that the player is betting
+	 * @return payout		payout resulting of the pair bet-handtype 
+	 */
+	
 	int getPayout(String handtype, int bet);
+	
+	/**
+	 * This method allows user to implement its own way of checking the best strategy to play a given hand. Returns 
+	 * an array with the positions that are advised to hold
+	 * @param hand			hand to be analyzed
+	 * @return positions	array with the positions of the cards in the hand that are advised to hold	
+	 */
+	
 	int[] getAdvice(Hand hand);
+	
+	/**
+	 * This method allows user to initialize a statistics structure as he/she wishes. An object of 
+	 * type Statistics is returned
+	 * @return statistic		 object of type Statistics initialized
+	 */
+	
 	Statistics initStatistics();
 
 }
